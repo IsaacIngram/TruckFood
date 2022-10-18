1032b4a22209ccf1f857312bf53b6a6aa430134c
IsaacIngram <isaac@isaacingram.com>
2022-10-18 15:50:07 -0400

    Initial commit

---
+++ src/web.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+
+
+def init_driver(browser: str, path: str = None) -> webdriver:
+    """
+    Initialize the webdriver using the specified browser at the given executable file
+    :param browser: The browser to use as a string. Currently accepted browsers are "safari", "chrome", "firefox", or
+    "chromium_edge"
+    :param path: The executable file path to this browser
+    :return: A Selenium webdriver
+    """
+    if browser == "safari":
+        if path is None:
+            return webdriver.Safari()
+        else:
+            return webdriver.Safari(executable_path=path)
+    elif browser == "chrome":
+        if path is None:
+            return webdriver.Chrome()
+        else:
+            return webdriver.Chrome(executable_path=path)
+    elif browser == "firefox":
+        if path is None:
+            return webdriver.Firefox()
+        else:
+            return webdriver.Firefox(executable_path=path)
+    elif browser == "chromium_edge":
+        if path is None:
+            return webdriver.ChromiumEdge()
+        else:
+            return webdriver.ChromiumEdge(executable_path=path)
+    else:
+        print("Error: Given browser '" + browser + "' not found. Supported browsers are 'safari', 'chrome', 'firefox',"
+                                                   "and 'chromium_edge'. Please use one of these and try again.")
+        exit()
+
