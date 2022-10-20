3b45f45d9d751fe5d84f87ffaa29845226971660
IsaacIngram <isaac@isaacingram.com>
2022-10-20 00:18:09 -0400

    Add "times edited this week" storage

--- src/data_storage.py
+++ src/data_storage.py
@@ -10,23 +10,30 @@ class DataStorage:
         """
         self.file_path = file_path
 
-    def write_data(self, data: str) -> None:
+    def write_data(self, data: str, times_this_week: int) -> None:
         """
         Write data to a datafile
         :param data: The data to write
+        :param times_this_week
         :return: None
         """
         with open(self.file_path, 'w') as file:
-            file.write(data)
+            file.write(str(data.__hash__()) + "\n" + str(times_this_week))
 
-    def read_data(self) -> str:
+    def read_data(self) -> (str, int):
         """
         Get data from the datafile
-        :return: A string containing the content of the file
+        :return: A string containing the first line of the file and an integer containing the second line
         """
         with open(self.file_path) as file:
             # Append all lines of the file to the same output string
             output: str = ""
-            for line in file:
-                output = output + "\n" + line
-            return output
+            return file.readline(0), int(file.readline(1))
+
+    def compare_data(self, current_data: str):
+        """
+        Compare a given string with the stored string
+        :param current_data: The string to compare
+        :return: Whether the strings are equal
+        """
+        return self.read_data()[0] == str(current_data.__hash__())
