84ff1558f5d3f4813e5faf9eff61a2ae19458ca7
IsaacIngram <isaac@isaacingram.com>
2022-10-22 18:51:05 -0400

    Rename to send message and add edit messaage

--- src/slack_bot.py
+++ src/slack_bot.py
@@ -8,6 +8,9 @@ class Bot:
     def __init__(self, token: str):
         self.client = WebClient(token)
 
-    def post_message(self, message: str):
+    def send_message(self, message: str):
         response = self.client.chat_postMessage(channel="#truckfood-bot", text=message)
         return response
+
+    def edit_message(self, new_message: str, timestamp: str):
+        self.client.chat_update(channel="#truckfood-bot", ts=timestamp, text=new_message)
